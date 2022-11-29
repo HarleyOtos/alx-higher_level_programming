652a17489a5ce0a8b34f7ed8c1de48ad4b39ecbe
Harley <111179132+HarleyOtos@users.noreply.github.com>
2022-11-30 00:36:18 +0100

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,5 +4,5 @@ for combine in range(0, 100):
     s2 = combine % 10
     if s1 < s2 and s1 != s2 and combine != 89:
         print('{:02d}, '.format(combine), end='')
-        if combine == 89:
-            print('{}'.format(combine))
+    if combine == 89:
+        print('{}'.format(combine))
