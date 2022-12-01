19c131819ca45ac0773dd9da018263d0c9452c52
Harley <111179132+HarleyOtos@users.noreply.github.com>
2022-12-01 21:38:39 +0100

    task 0 - 5

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == '__main__':
+    s = sys.argv
+    l_s = len(s)
+    sum = 0
+
+    if l_s > 1:
+        for i in range(1, l_s):
+            sum += int(s[i])
+            print(sum)
