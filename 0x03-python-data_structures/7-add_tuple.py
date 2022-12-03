08413c3f1379745309d90d235d8bef6180570dec
Harley <111179132+HarleyOtos@users.noreply.github.com>
2022-12-03 22:45:29 +0100

    Update 7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,12 +4,10 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_a = (0, 0)
     elif len(tuple_a) == 1:
         tuple_a = (tuple_a[0], 0)
-
     if len(tuple_b) == 0:
         tuple_b = (0, 0)
     elif len(tuple_b) == 1:
         tuple_b = (tuple_b[0], 0)
-
     last_tuple = (int(tuple_a[0]) + int(tuple_b[0]),
                    int(tuple_a[1]) + int(tuple_b[1]))
     return last_tuple
