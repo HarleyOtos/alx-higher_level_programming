c7e48d3d016dda772fb04c90164bef24fd1c4916
Harley <111179132+HarleyOtos@users.noreply.github.com>
2022-12-19 21:55:12 +0100

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
-    idx = 0
 
     try:
+        idx = 0
         for i in my_list:
             if idx < x:
-                print('{}'.format(my_list[idx]), end='')
+                print('{}'.format(i), end='')
                 idx += 1
-
-        print()
-    except TypeError:
-        pass
-    finally:
+        print('')
         return idx
+    except SyntaxError:
+        print('Error Syntax')
+
