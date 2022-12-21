259dc5dd751a8a8476a0ddc5ac20e887df17ba39
Harley <111179132+HarleyOtos@users.noreply.github.com>
2022-12-21 23:41:19 +0100

    Update 5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -36,14 +36,14 @@ class Square:
         """__init__
         The size setter method update the size
         value of the square.
-        
+
         Attributes:
             size (:obj:`int`): The new size of the square.
-        
+
         Raises:
             TypeError: If `size` type is not `int`.
             ValueError: If `size` is less than `0`.
-        
+
         """
         if type(size) is not int:
             raise TypeError('size must be an integer')
