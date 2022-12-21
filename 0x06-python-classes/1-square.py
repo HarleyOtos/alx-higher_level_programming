88e45b1ed52dae4d3da5c196472b42b6f50f0bc8
Harley <111179132+HarleyOtos@users.noreply.github.com>
2022-12-21 22:49:43 +0100

    docstring fix

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 """Square Class
-A Square Class
+A Square Class with method
 """
 
 
 class Square:
     """Square class
     """
-
     def __init__(self, size):
         """__init__
             The __init__ method initializes the size value
             of the square class.
-        
+
         Attributes:
             size (int): The size of the square.
+
         """
         self.__size = size
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Square Class"""
+"""Square Class
+A Square Class with method
+"""
 
 
 class Square:
