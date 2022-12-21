9db4238a9160bcfddd73dada3ed978f5b91b22b6
Harley <111179132+HarleyOtos@users.noreply.github.com>
2022-12-21 22:40:49 +0100

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -8,15 +8,12 @@ class Square:
     """Square class
     """
 
-
     def __init__(self, size):
         """__init__
-        The __init__ method initializes the size value
-        of the square class.
+            The __init__ method initializes the size value
+            of the square class.
         
         Attributes:
-        size (int): The size of the square.
+            size (int): The size of the square.
         """
-
-
         self.__size = size
