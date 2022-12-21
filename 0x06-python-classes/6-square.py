1e14c81ebd90e615ab07c1da7db4a4a3a867210a
Harley <111179132+HarleyOtos@users.noreply.github.com>
2022-12-22 00:14:47 +0100

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,13 +5,11 @@ This module contains the `Square` class and its methods.
 
 
 class Square:
-    """
-    Square Class
+    """Square Class
     This class represents a square with a size and a position.
     """
     def __init__(self, size=0, position=(0, 0)):
-        """
-        Initialize a square instance.
+        """Initialize a square instance.
 
         Args:
             size (int, optional): The size of the square. Default is 0.
@@ -21,6 +19,7 @@ class Square:
             TypeError: If `size` type is not `int`.
             ValueError: If `size` is less than `0`.
             TypeError: If `position` is not a tuple of 2 positive integers.
+
         """
         if type(size) is not int:
             raise TypeError('size must be an integer')
@@ -44,8 +43,7 @@ class Square:
 
     @size.setter
     def size(self, size):
-        """
-        Set the size of the square.
+        """Set the size of the square.
 
         Args:
             size (int): The new size of the square.
@@ -53,6 +51,7 @@ class Square:
         Raises:
             TypeError: If `size` type is not `int`.
             ValueError: If `size` is less than `0`.
+
         """
         if type(size) is not int:
             raise TypeError('size must be an integer')
@@ -69,14 +68,14 @@ class Square:
 
     @position.setter
     def position(self, position):
-        """
-        Set the position of the square.
+        """Set the position of the square.
 
         Args:
             position (tuple): The new position of the square.
 
         Raises:
             TypeError: If `position` is not a tuple of 2 positive integers.
+
         """
         if self.__check_tuple(position) is False \
             or self.__check_indexes(position) is False \
@@ -87,14 +86,14 @@ class Square:
         self.__position = position
 
     def __check_tuple(self, position):
-        """
-        Check if `position` is a tuple.
+        """Check if `position` is a tuple.
 
         Args:
             position (tuple): The position to check.
 
         Returns:
             bool: True if `position` is a tuple, False otherwise.
+
         """
         if type(position) is tuple:
             return True
@@ -102,14 +101,14 @@ class Square:
         return False
 
     def __check_indexes(self, position):
-        """
-        Check if `position` has 2 elements.
+        """Check if `position` has 2 elements.
 
         Args:
             position (tuple): The position to check.
 
         Returns:
             bool: True if `position` has 2 elements, False otherwise.
+
         """
         if len(position) == 2:
             return True
@@ -117,14 +116,14 @@ class Square:
         return False
 
     def __check_integers(self, position):
-        """
-        Check if the elements of `position` are integers.
+        """Check if the elements of `position` are integers.
 
         Args:
             position (tuple): The position to check.
 
         Returns:
             bool: True if the elements of `position` are integers, False otherwise.
+
         """
         if type(position[0]) is int and type(position[1]) is int:
             return True
@@ -132,14 +131,14 @@ class Square:
         return False
 
     def __check_values(self, position):
-        """
-        Check if the elements of `position` are positive.
+        """Check if the elements of `position` are positive.
 
         Args:
             position (tuple): The position to check.
 
         Returns:
             bool: True if the elements of `position` are positive, False otherwise.
+
         """
         if position[0] >= 0 and position[1] >= 0:
             return True
@@ -147,19 +146,19 @@ class Square:
         return False
 
     def area(self):
-        """
-        Get the area of the square.
+        """Get the area of the square.
 
         Returns:
             int: The area of the square.
+
         """
         return self.__size ** 2
 
     def my_print(self):
-        """
-        Print the square using the `#` character.
+        """Print the square using the `#` character.
 
         If the size is 0, print an empty line.
+
         """
         if self.__size == 0:
             print()
