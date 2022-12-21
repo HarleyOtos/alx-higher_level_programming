14edbaac5f33458f14e29e82c3b7a6d7b1ec9c8a
Harley <111179132+HarleyOtos@users.noreply.github.com>
2022-12-21 21:52:45 +0100

    pycodestyle fix

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Square Class
-This class don't do anything for the moment
+An empty class that does nothing at the moment
 """
 
-
 class Square:
+    """Empty class"""
     pass
