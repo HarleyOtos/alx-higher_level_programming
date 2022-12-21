5af739623ab0bd1001bf06a41685cfd4b0914002
Harley <111179132+HarleyOtos@users.noreply.github.com>
2022-12-21 23:34:18 +0100

    Update 5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -58,8 +58,6 @@ class Square:
         return self.__size ** 2
 
     def my_print(self):
-        """Print the square area
-        """
         if self.__size == 0:
             print()
             return None
