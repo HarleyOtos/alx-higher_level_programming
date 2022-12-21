9bf8a5b035dd92284219cfe5721b689007ea2bac
Harley <111179132+HarleyOtos@users.noreply.github.com>
2022-12-22 00:25:51 +0100

    advanced task

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+import dis
+import math
+
+class MagicClass:
+    """
+    MagicClass
+
+    This class represents a circle with a radius.
+    """
+    def __init__(self, radius=0):
+        """
+        Initialize a `MagicClass` instance.
+
+        Args:
+            radius (int or float, optional): The radius of the circle. Default is 0.
+
+        Raises:
+            TypeError: If `radius` is not a number.
+        """
+        self.__radius = 0
+
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError('radius must be a number')
+
+        self.__radius = radius
+
+    def area(self):
+        """
+        Get the area of the circle.
+
+        Returns:
+            float: The area of the circle.
+        """
+        return self.__radius ** 2 * math.pi
+
+    def circumference(self):
+        """
+        Get the circumference of the circle.
+
+        Returns:
+            float: The circumference of the circle.
+        """
+        return 2 * math.pi * self.__radius
