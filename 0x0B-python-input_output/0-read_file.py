b82f4442c8525920c313f1482f146f73419b5594
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-01-11 22:53:02 +0100

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+A module that reads text file (UTF8) and prints it to stdout
+"""
+
+
 def read_file(filename=""):
     """ function that reads a text file (UTF8) and prints it to stdout """
 
