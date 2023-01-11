f423f4beb09f16e75fa84f2e6c6d7eb41376a49c
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-01-11 21:07:10 +0100

    Update 10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -3,6 +3,7 @@
 Class that inherits from BaseGeometry
 """
 
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 Rectangle = __import__('9-rectangle').Rectangle
 
@@ -11,6 +12,7 @@ class Square(Rectangle):
     """
     A class of square that inherits from Rectangle
     """
+
     def __init__(self, size):
         """ A contructor """
         self.integer_validator("size", size)
