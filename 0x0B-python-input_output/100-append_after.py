951dcc4e2b56e049790ae69128201a9d014ed0ca
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-01-12 09:25:09 +0100

    advanced tasks

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+A function that insert a line of text to a file
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """The statement"""
+    with open(filename, mode='r', encoding='utf-8') as f:
+        text = f.readlines()
+        new_text = []
+
+        for line in text:
+            new_text.append(line)
+
+            if search_string in line:
+                new_text.append(new_string)
+
+    with open(filename, mode='w', encoding='utf-8') as nf:
+        for line in new_text:
+            nf.write(line)
