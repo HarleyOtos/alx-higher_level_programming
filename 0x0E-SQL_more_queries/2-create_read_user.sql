e29a8a3e3d8dea968792e26eb2ceff3e1bbfd410
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-02-16 23:56:22 +0100

    error fix

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,8 +1,8 @@
 -- creates the database 'hbtn_0d_2' and the user 'user_0d_2'
 -- 'user_0d_2' only SELECT privilege in 'hbtn_0d_2'
 -- 'user_0d_2' password set to 'user_0d_2_pwd'
-CREATE DATABASE IF EXISTS hbtn_0d_2;
-DROP USER 'user_0d_2'@'localhost';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+DROP USER IF 'user_0d_2'@'localhost';
 CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
