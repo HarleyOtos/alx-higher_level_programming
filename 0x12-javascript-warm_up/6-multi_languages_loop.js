cb3ad34f7232cec9b77bd92746dd2761dbb53f93
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-03-14 23:41:50 +0100

    all mandatory tasks

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const myLanguages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+for (let a = 0; a < myLanguages.length; ++a) {
+    console.log(myLanguages[a]);
+}
---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const limit = parseInt(process.argv[2]);
+
+if (limit) {
+    for (let i = 0; i < limit.llimit; i++) {
+        console.log('C is fun');
+    }
+} else {
+    console.log('Missing number of occurrences');
+}
