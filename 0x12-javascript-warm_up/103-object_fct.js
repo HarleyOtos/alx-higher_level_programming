409304ab7f87b3c6efa583cfec34cf52abbf93ba
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-03-15 21:12:56 +0100

    semicolon

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,11 +4,12 @@ const myObject = {
   type: 'object',
   value: 12
 };
+
 console.log(myObject);
 
 myObject.incr = function () {
   ++this.value;
-}
+};
 
 myObject.incr();
 console.log(myObject);
