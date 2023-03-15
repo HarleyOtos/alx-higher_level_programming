43f2aca154696953f044f8c2d4669a9c98e4ae3a
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-03-15 20:25:36 +0100

    standard style fix

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,10 +3,10 @@
 const n = parseInt(process.argv[2]);
 
 function factorial(n) {
-    if (n === 0 || isNaN(n)) {
-        return 1;
-    }
-    return n * factorial(n - 1);
+  if (n === 0 || isNaN(n)) {
+    return 1;
+  }
+  return n * factorial(n - 1);
 }
 
 console.log(factorial(n));
