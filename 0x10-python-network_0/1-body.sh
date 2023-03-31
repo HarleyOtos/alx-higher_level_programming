defa15b9f3b4fbe65646be08b99ba817af0e2cf1
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-04-01 00:47:54 +0100

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script takes a URL, sends a GET request to it using curl with silent mode, and displays the body of the response if the status code is 200.
-if [[ $(curl -s -o /dev/null -w "%{http_code}" "$1") == "200" ]]; then curl -s "$1"; fi
+# This script takes a URL, sends a GET request to it using curl with -s mode, & displays the body of the response if the status code is 200.
+curl -sI "$1" | grep "200 OK" > /dev/null && curl -sL "$1" || true
