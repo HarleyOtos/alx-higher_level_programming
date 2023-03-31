ed8d4e2e5f4625b0e073176340e0085338cbcebc
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-04-01 00:35:52 +0100

    Formatted to fix 'if' error to contain in 3 line limit

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes a URL, sends a GET request to it using curl with silent mode, and displays the body of the response if the status code is 200.
-response=$(curl -s -o /dev/null -w "%{http_code}" "$1") if [ "$response" == "200" ]; then curl -s "$1" fi
+if [[ $(curl -s -o /dev/null -w "%{http_code}" "$1") == "200" ]]; then curl -s "$1"; fi
