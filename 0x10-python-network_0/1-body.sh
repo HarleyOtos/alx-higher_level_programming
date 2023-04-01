aa5241f348adcfe9579a192478f2341ea70e6f69
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-04-01 17:16:48 +0100

    fixed initial error causing checker fail

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A script that takes a URL, sends a GET request to it using curl, & displays the body of the response.
-curl -s -o /dev/null -w "%{http_code}" "$1" | grep "200" > /dev/null && curl -sL "$1" || true
+curl -sL -w "%{http_code}" "$1" | awk '$NF == 200 {getline; print}'
