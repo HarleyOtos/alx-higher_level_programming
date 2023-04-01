76a4e37bfb0cabe5a22bc6700949d4e71357cfb4
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-04-01 17:04:47 +0100

    Error fix

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes in a URL & displays all `HTTP` methods the server will accept.
-curl -sI "$1" | grep -i allow | awk '{print $2,$3,$4}'
+curl -sI "$1" | awk '/Allow/{gsub(/Allow: /,"");print}'
