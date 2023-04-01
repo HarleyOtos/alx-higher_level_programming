1e05fa828205c042a0541d3268197eeba1e06633
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-04-01 16:57:03 +0100

    fix code error that has to do with sI option which only send request and not GET request

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A script that takes a URL, sends a GET request to it using curl, & displays the body of the response.
-curl -sI "$1" | grep "200 OK" > /dev/null && curl -sL "$1" || true
+curl -s -o /dev/null -w "%{http_code}" "$1" | grep "200" > /dev/null && curl -sL "$1" || true
