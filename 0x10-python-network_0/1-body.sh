9738e4b64d440563ff20a32ab1156f6c801f4d42
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-04-01 17:25:38 +0100

    Checker error fix

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A script that takes a URL, sends a GET request to it using curl, & displays the body of the response.
-curl -sL -w "%{http_code}" "$1" | awk '$NF == 200 {getline; print}'
+curl -sL "$1" -X GET -o /dev/null -w "%{http_code}" | grep "200" > /dev/null && curl -sL "$1" || true
