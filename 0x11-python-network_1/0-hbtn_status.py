5fc5e77484c6e6304e42cb3a3cab5d3c1c08fe2d
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-04-04 21:31:33 +0100

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
+"""A script to fetch "https://alx-intranet.hbtn.io/status"
 """
-A script to fetch "https://alx-intranet.hbtn.io/status"
-"""
-
 
 import urllib.request
 
+
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
+
     with urllib.request.urlopen(url) as response:
         content = response.read()
         utf8_content = content.decode('utf8')
