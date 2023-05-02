c3c145e44840ddab8bc31a5402eea57478ced7d6
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-05-02 20:01:04 +0100

    Update 5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -7,7 +7,7 @@ const url = process.argv[2];
 const filePath = process.argv[3];
 
 request(url, function (_err, _response, body) {
-  fs.writeFile(filePath, body, 'utf-8', function (err) {
+  fs.writeFile(filePath, body, 'utf8', function (err) {
     if (err) {
       console.log(err);
     }
