c1d4726624cbb708b9204c648232d4f928e89515
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-05-02 20:19:34 +0100

    issues fix

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,19 +3,21 @@
 const request = require('request');
 
 const url = process.argv[2];
+const url = `https://swapi-api.alx.tools/api/films/${movieId}/`;
 
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
   } else {
-    const films = JSON.parse(body).results;
-    let count = 0;
-    for (const film of films) {
-      const characters = film.characters;
-      if (characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
-        count++;
-      }
-    }
-    console.log(count);
+    const charactersUrls = JSON.parse(body).characters;
+    charactersUrls.forEach(url => {
+      request(url, function (error, response, body) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    });
   }
 });
