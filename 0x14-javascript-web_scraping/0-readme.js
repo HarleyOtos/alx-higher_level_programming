ab12a61dddce20b1ec93c655720dd9ea981fb871
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-05-02 18:09:48 +0100

    typo fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 const filePath = process.argv[2];
 
-fs.readFile(filePath, 'uft-8', function (err, data) {
+fs.readFile(filePath, 'utf-8', function (err, data) {
     if (err) {
         console.log(err);
     } else {
