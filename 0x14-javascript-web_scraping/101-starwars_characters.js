10f8e11438028fb8533e8e0dcc85a0079e0ba216
root <harleyotos@gmail.com>
2023-05-02 12:51:47 -0700

    whitespace fix

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -6,30 +6,30 @@ const movieId = process.argv[2];
 const url = `https://swapi.dev/api/films/${movieId}/`;
 
 request(url, (error, response, body) => {
-    if (error) {
-      console.error(error);
-      return;
-    }
-  
-    const charactersUrls = JSON.parse(body).characters;
-    const charactersList = [];
-  
-    let requestsLeft = charactersUrls.length;
-  
-    charactersUrls.forEach((charUrl) => {
-      request(charUrl, (err, _response, charBody) => {
-        if (err) {
-          console.error(err);
-          return;
-        }
-  
-        const characterName = JSON.parse(charBody).name;
-        charactersList.push(characterName);
-        requestsLeft--;
-  
-        if (requestsLeft === 0) {
-          console.log(charactersList.join('\n'));
-        }
-      });
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const charactersUrls = JSON.parse(body).characters;
+  const charactersList = [];
+
+  let requestsLeft = charactersUrls.length;
+
+  charactersUrls.forEach((charUrl) => {
+    request(charUrl, (err, _response, charBody) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      const characterName = JSON.parse(charBody).name;
+      charactersList.push(characterName);
+      requestsLeft--;
+
+      if (requestsLeft === 0) {
+        console.log(charactersList.join('\n'));
+      }
     });
   });
+});
