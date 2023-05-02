35b59baf8bcc37c8f00177b4cd48a3db58d07a12
Harley <111179132+HarleyOtos@users.noreply.github.com>
2023-05-02 20:40:00 +0100

    a fix

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,33 +3,33 @@
 const request = require('request');
 
 const movieId = process.argv[2];
-const url = `https://swapi.dev/api/films/${movieId}`;
+const url = `https://swapi.dev/api/films/${movieId}/`;
 
-request(url, (err, response, body) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-
-  const charactersUrlList = JSON.parse(body).characters;
-  const charactersList = [];
-
-  let requestsLeft = charactersUrlList.length;
-
-  charactersUrlList.forEach((characterUrl) => {
-    request(characterUrl, (err, response, body) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-
-      charactersList.push(JSON.parse(body).name);
-      requestsLeft--;
-
-      if (requestsLeft === 0) {
-        // all requests have finished
-        console.log(charactersList.join('\n'));
-      }
+request(url, (error, response, body) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+  
+    const charactersUrls = JSON.parse(body).characters;
+    const charactersList = [];
+  
+    let requestsLeft = charactersUrls.length;
+  
+    charactersUrls.forEach((charUrl) => {
+      request(charUrl, (err, _response, charBody) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+  
+        const characterName = JSON.parse(charBody).name;
+        charactersList.push(characterName);
+        requestsLeft--;
+  
+        if (requestsLeft === 0) {
+          console.log(charactersList.join('\n'));
+        }
+      });
     });
   });
-});
--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,22 +2,20 @@
 
 const request = require('request');
 
-const url = process.argv[2];
-const url = `https://swapi-api.alx.tools/api/films/${movieId}/`;
+const url = 'https://swapi-api.alx-tools.com/api/films/';
 
-request(url, function (error, response, body) {
-  if (error) {
-    console.log(error);
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
   } else {
-    const charactersUrls = JSON.parse(body).characters;
-    charactersUrls.forEach(url => {
-      request(url, function (error, response, body) {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    });
+    const films = JSON.parse(body).results;
+    let count = 0;
+    for (const film of films) {
+      const characters = film.characters;
+      if (characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+        count++;
+      }
+    }
+    console.log(count);
   }
 });
